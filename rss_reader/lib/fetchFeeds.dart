e284bbaf105427427f7266e877e7ec7a75bcdbbc
Jan Barfuß <janbar2001@gmail.com>
2023-12-19 12:55:46 +0100

    bit fetch

--- rss_reader/lib/fetchFeeds.dart
+++ rss_reader/lib/fetchFeeds.dart
@@ -4,24 +4,33 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-Future<String> fetch() async {
-    final response = await http
-          .get(Uri.parse('https://www.dkriesel.com/feed.php?linkto=current&content=html&mode=blogtng&blog=blog-de'));
+//https://flutterxperts.com/how-to-solve-flutter-web-api-cors-error-with-dart-code/
+class CorsMiddleware extends http.BaseClient {
+  final http.Client _inner = http.Client();
 
-      if (response.statusCode == 200) {
-        // If the server did return a 200 OK response,
-        // then parse the JSON.
-        return (response.body) as String;
-      } else {
-        // If the server did not return a 200 OK response,
-        // then throw an exception.
-        throw Exception('Failed to load album');
-      }
+  @override
+  Future<http.StreamedResponse> send(http.BaseRequest request) {
+    request.headers['Access-Control-Allow-Origin'] = '*';
+    request.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE';
+    request.headers['Access-Control-Allow-Headers'] =
+        'Origin, Content-Type, X-Auth-Token';
+
+    print('worked');
+    return _inner.send(request);
+  }
+}
+
+final client = http.Client();
+
+Future<void> fetchData() async {
+  final request = http.Request('GET', Uri.parse('https://www.dkriesel.com/feed.php?linkto=current&content=html&mode=blogtng&blog=blog-de'));
+  final streamedResponse = await client.send(request);
+  final response = await http.Response.fromStream(streamedResponse);
+
+  // Process the response
+  print(response.body);
 }
 
 void main(){
-      fetch().then((result) {
-           if (result is String)
-                print(result); //use toString to convert as String
-    });  
+    fetchData();
 }
